8e3ac3841e4a3d4a57e861e870b1fbfc28621728
Stuart <stuart@calyxagency.com>
2015-05-07 15:51:28 +0200

    helpers for controls manipulation

---
+++ core/helpers/controls.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Layers Controls helper functions
+ *
+ * This file is has helper funcitons to help with the handling of the customizer controls.
+ *
+ * @package Layers
+ * @since Layers 1.1.2
+ */
+
